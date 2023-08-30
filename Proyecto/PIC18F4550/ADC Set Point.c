d6a83b3a89514b6ef830226f3a9cfc08631e0477
vhperaltaj <victor.peralta@microside.com>
2023-08-30 14:07:49 -0600

    Updated identation, added gitignore

--- Proyecto/PIC18F4550/ADC Set Point.c
+++ Proyecto/PIC18F4550/ADC Set Point.c
@@ -13,40 +13,29 @@ un Set Point, el LED encenderá.
 ---------------------------------------------------------------------------
 */
 
+#include <18F4550.h>                           // Incluye el microcontrolador con el que se va a trabajar 
+#DEVICE ADC = 8                                // Configura el ADC a 8 bits
+#use delay( clock=48Mhz, crystal )             // Tipo de oscilador y frecuencia dependiendo del microcontrolador 
+#build( reset=0x02000, interrupt=0x02008 )     // Asigna los vectores de reset e interrupción para la versión con bootloader
+#org 0x0000,0x1FFF {}                          // Reserva espacio en memoria para el bootloader
 
-#include <18F4550.h>                            //Incluye el microcontrolador con el que se va a trabajar 
-#DEVICE ADC=8                                   //Configura el ADC a 8 bits
-#use delay(clock=48Mhz, crystal)                //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
-#build(reset=0x02000,interrupt=0x02008)         //Asigna los vectores de reset e interrupción para la versión con bootloader
-#org 0x0000,0x1FFF {}                           //Reserva espacio en memoria para el bootloader
+#define LED PIN_A4                             // Pin donde está conectado el LED del X-TRAINER
 
-#define LED PIN_A4                              //Pin donde está conectado el LED del X-TRAINER
+int ValorPot;                                  // Variable para almacenar el valor leído del ADC
 
-int ValorPot;                                   //Variable para almacenar el valor leído del ADC
+void main( void ) {
+   setup_oscillator( OSC_8MHZ );               // Configura oscilador interno
+   setup_adc_ports( AN0 );                     // Configura solo el puerto A0 como entrada analógica
+   setup_adc( ADC_CLOCK_DIV_8 );               // Configura oscilador para la conversión
 
-void main()
+   while ( 1 ) {
+      set_adc_channel(0);                      // Selecciona el canal 0 para la conversión
+      ValorPot = read_adc();                   // Lee el valor del ADC y lo guarda en la variable
 
-{
-
-   setup_oscillator(OSC_8MHZ);                    //Configura oscilador interno
-   setup_adc_ports (AN0);                         //Configura solo el puerto A0 como entrada analógica
-   setup_adc (ADC_CLOCK_DIV_8);                   //Configura oscilador para la conversión
-
-   while (1)
-   {
-
-       set_adc_channel (0);                 //Selecciona el canal 0 para la conversión
-
-       ValorPot = read_adc ();              //Lee el valor del ADC y lo guarda en la variable
-
-      if (ValorPot >= 128)                  //Compara el valor de el ADC con 128
-             {
-                     output_HIGH (LED);     // Leds activos
-             }
-
-      else
-             {
-                    output_LOW (LED);       // Leds activos
-             }
+      if ( ValorPot >= 128 ) {                 // Compara el valor de el ADC con 128
+         output_HIGH( LED );                   // Leds activos
+      } else {
+         output_LOW( LED );                    // Leds activos
+      }
    }
 }
